7ca7ae085a5cf630a267807d5d0e1744ab699963
Nous <nousstudios72@gmail.com>
2026-05-11 20:28:33 -0300

    Troquei o título da página

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Direita leva ao nazismo'),
+      home: const MyHomePage(title: 'Página de Login'),
     );
   }
 }
